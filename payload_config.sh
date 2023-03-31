3401e6f2888b8071931e263f6a89bf3726667e73
huiminlim <e0036379@u.nus.edu>
2023-03-31 12:36:42 +0800

    Update payload_config.sh

--- payload_config.sh
+++ payload_config.sh
@@ -4,4 +4,4 @@ pip3 install apscheduler
 pip3 install pyserial
 pip3 install reedsolo
 
-cd Desktop && git clone https://github.com/huiminlim/payload_manager-lite
+cd Desktop && git clone https://github.com/dream2space/payload_manager-lite
